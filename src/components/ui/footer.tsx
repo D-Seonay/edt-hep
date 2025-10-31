5ae66527163c9be3ca3fd1ef9cb62a02b7233594
Many0nne <ptitesaile@gmail.com>
2025-10-31 13:16:40 +0100

    Fix: unfix footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 
   return (
-    <footer className="bg-muted/30 text-muted-foreground py-4 mt-6 rounded-t-2xl border-t border-border/50 text-center fixed bottom-0 w-full">
+    <footer className="bg-muted/30 text-muted-foreground py-4 mt-6 rounded-t-2xl border-t border-border/50 text-center bottom-0 w-full">
       <div className="text-xs mb-1">
         &copy; {new Date().getFullYear()} EDT C&amp;D - Dev par <a href="https://linkedin.com/in/matheo-delaunay" className="underline hover:text-accent" target="_blank" rel="noreferrer">Mathéo </a>
       </div>
